9b8fab6152f01f1cdf4406b46bdf865dc9e9c115
James Parrish <jthomasparrishjr@gmail.com>
2022-05-22 16:05:42 -0500

    commit tree eb5ebcab61536d570d2053147b1698541b65e3c0

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+return `This Saturday, I want to ${activity}!`
+}
+let mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(parentParam = '*'){
+    let innerFunction = function(param = 'special'){
+        return `You are ${parentParam}${param}${parentParam}!`
+    }
+    return innerFunction;
+}
+// let wrapAdjective = function(style="*") {
+//     return function(adjective="special") {
+//       return `You are ${style}${adjective}${style}!`
+//     }
+//   }
